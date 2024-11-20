56983493dbae563803cf2c0de9aee90d4574cf2b
Eldintaro Farrandi <eldintarofarrandi@gmail.com>
2024-11-20 19:38:41 +0700

    webserver dbserver done :)

--- main.tf
+++ main.tf
@@ -62,32 +62,50 @@ resource "libvirt_domain" "webserver" {
   }
 }
 
-# Database Server VM
+# Database Server VM Cloud-Init Configuration
 data "template_file" "dbserver_user_data" {
-  template = <<EOF
-#cloud-config
-hostname: dbserver
-users:
-  - name: ansible
-    sudo: ['ALL=(ALL) NOPASSWD:ALL']
-    shell: /bin/bash
-    ssh_authorized_keys:
-      - ${file("~/.ssh/id_rsa.pub")}
-EOF
+  template = <<-EOF
+    #cloud-config
+    hostname: dbserver
+    users:
+      - name: ansible
+        sudo: ['ALL=(ALL) NOPASSWD:ALL']
+        shell: /bin/bash
+        ssh_authorized_keys:
+          - ${file("~/.ssh/id_rsa.pub")}
+    runcmd:
+      - apt-get update && apt-get install -y cloud-guest-utils
+      - growpart /dev/vda 1
+      - resize2fs /dev/vda1
+    EOF
 }
 
-resource "libvirt_cloudinit_disk" "dbserver_cloudinit" {
-  name     = "dbserver-cloudinit.iso"
-  pool     = "default"
-  user_data = data.template_file.dbserver_user_data.rendered
-}
-
+# Create disk volume for dbserver
 resource "libvirt_volume" "dbserver_qcow2" {
   name           = "dbserver.qcow2"
   base_volume_id = libvirt_volume.ubuntu_image.id
   pool           = "default"
 }
 
+# Resize the disk after it is created
+resource "null_resource" "resize_dbserver_disk" {
+  depends_on = [libvirt_volume.dbserver_qcow2]
+
+  provisioner "local-exec" {
+    command = "sudo qemu-img resize /var/lib/libvirt/images/dbserver.qcow2 +10G"
+  }
+}
+
+# Create the Cloud-init Disk for dbserver
+resource "libvirt_cloudinit_disk" "dbserver_cloudinit" {
+  name      = "dbserver-cloudinit.iso"
+  pool      = "default"
+  user_data = data.template_file.dbserver_user_data.rendered
+
+  depends_on = [null_resource.resize_dbserver_disk]
+}
+
+# Create dbserver domain (VM)
 resource "libvirt_domain" "dbserver" {
   name   = "dbserver"
   memory = 8192
@@ -96,14 +114,17 @@ resource "libvirt_domain" "dbserver" {
   cloudinit = libvirt_cloudinit_disk.dbserver_cloudinit.id
 
   network_interface {
-    network_name = "default"
+    network_name   = "default"
     wait_for_lease = true
   }
 
   disk {
     volume_id = libvirt_volume.dbserver_qcow2.id
   }
+
+  depends_on = [libvirt_cloudinit_disk.dbserver_cloudinit]
 }
+
 
 # Outputs
 output "webserver_ip" {
