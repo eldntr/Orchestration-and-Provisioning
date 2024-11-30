a2fd0df35c2f9740d9a1794b28e16437c1e5f00e
Eldintaro Farrandi <eldintarofarrandi@gmail.com>
2024-11-30 11:12:11 +0700

    fix 2_

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 tofu init
 ./cleanup.sh
-ansible-playbook -i inventory.ini configure_webserver.yml 
-ansible-playbook -i inventory.ini configure_dbserver.yml 
+ansible-playbook -i inventory.ini installation/configure_webserver.yml 
+ansible-playbook -i inventory.ini installation/configure_dbserver.yml 
